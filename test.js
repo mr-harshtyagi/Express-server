52a0494c5b81c641d2f0ecf1be761a932165c270
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-06-30 15:22:15 +0530

    commit on linux after long time

--- test.js
+++ test.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const app = express()
-const port = 3000
+app.use(express.json())
+const port = 4000
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bigchain', { useNewUrlParser: true });
+
+const assetSchema={
+  data:Object,
+  id:String
+}
+const transactionSchema={
+  
+}
+const Asset = mongoose.model('Asset', assetSchema)
+const Transaction = mongoose.model('Transaction', transactionSchema)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  Asset.find(function(err, foundAssets){
+    if(err)
+    res.send(err)
+    else
+    {
+    res.send(foundAssets)
+    }
+    
+  })
+  
+})
+
+app.get('/transactions', (req, res) => {
+
+  Transaction.find(function(err, foundTxns){
+    if(err)
+    res.send(err)
+    else
+    res.send(foundTxns)
+  })
 })
 
 app.listen(port, () => {
